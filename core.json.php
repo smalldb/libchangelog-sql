872d899915c87c03f5357f710ab94772f653df21
Josef Kufner <josef@kufner.cz>
2015-02-10 12:38:46 +0100

    Clean up after plugin

--- core.json.php
+++
@@ -1,9 +0,0 @@
-{
-    "_": "<?php printf('_%c%c}%c',34,10,10);__halt_compiler();?>",
-    "changelog_sql": {
-        "database_resource": "database",
-        "changelog_dir": "app/database/changelog.sql",
-        "changelog_table": "about_changelog"
-    }
-}
-
